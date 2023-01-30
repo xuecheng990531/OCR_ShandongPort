c65bbcba262b5d9f5fce502174046bf4ac81a3e0
xuecheng990531 <leexuecheng2017@outlook.com>
2023-01-30 12:04:25 +0800

    重新格式化app.py

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import os
 import uvicorn
 from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi import FastAPI,UploadFile,File,applications
+from fastapi import FastAPI, UploadFile, File, applications
 from typing import Optional
 from component_modules.autodel import autodel
 from component_modules.autils import *
+
+
 def swagger_monkey_patch(*args, **kwargs):
     return get_swagger_ui_html(
         *args, **kwargs,
         swagger_js_url='https://cdn.bootcdn.net/ajax/libs/swagger-ui/4.10.3/swagger-ui-bundle.js',
         swagger_css_url='https://cdn.bootcdn.net/ajax/libs/swagger-ui/4.10.3/swagger-ui.css'
     )
+
+
 applications.get_swagger_ui_html = swagger_monkey_patch
 
-app = FastAPI(title='光学字符识别项目',description='根据每个单据要求的关键字进行返回')
+app = FastAPI(title='光学字符识别项目', description='根据每个单据要求的关键字进行返回')
 
-@app.post('/ocr',tags=["识别接口（POST方法）"])
-async def ocr(ID:int,Type: Optional[str] = None,File: UploadFile=File(...)):
+
+@app.post('/ocr', tags=["识别接口（POST方法）"])
+async def ocr(ID: int, Type: Optional[str] = None, File: UploadFile = File(...)):
     '''    
     OCR识别    
     - 参数 ID: 上传哪类单据    
@@ -64,46 +69,43 @@ async def ocr(ID:int,Type: Optional[str] = None,File: UploadFile=File(...)):
     - ID=14--------------------------->过磅单（特定公司）
 
     - ID=15--------------------------->集装箱信息
-    '''    
-    filename=File.filename
+    '''
+    filename = File.filename
     extension = os.path.splitext(File.filename)[-1]
-    name=os.path.splitext(File.filename)[0]
-    save_path=os.path.join('save_files',filename)
+    name = os.path.splitext(File.filename)[0]
+    save_path = os.path.join('save_files', filename)
 
-    if extension not in imgType_list and extension!='.pdf':
-        return{"上传错误":'上传的文件不在可上传范围内'}
+    if extension not in imgType_list and extension != '.pdf':
+        return {"上传错误": '上传的文件不在可上传范围内'}
     else:
         # 如果图片保存文件夹中的数量太多，进行删除
-        if len(os.listdir('save_files'))>20:
+        if len(os.listdir('save_files')) > 20:
             autodel('save_files')
 
         # 写入图片
         await save_img(File, filename)
 
-        
         # 检查上传的文件扩展名
         if extension in imgType_list:
-            if ID==7:
+            if ID == 7:
                 check(img_path=save_path)
-            elif ID==12:
-                save_path=process_ID12(save_path)
-                cv2.imwrite('new.png',save_path)
-            pos,value=detect_img(save_path)
+            elif ID == 12:
+                save_path = process_ID12(save_path)
+                cv2.imwrite('new.png', save_path)
+            pos, value = detect_img(save_path)
 
-            return detect_value(pos,ID,value,Type,save_path,filename)
+            return detect_value(pos, ID, value, Type, save_path, filename)
 
         else:
-            count,img_list=pdf_img(save_path,name)
+            count, img_list = pdf_img(save_path, name)
 
-            if ID==7:
+            if ID == 7:
                 check(img_path=img_list[0])
 
-            pos,value=detect_pdf(img_list,count)
-            
-            return detect_value(pos,ID,value,Type,save_path,filename)
+            pos, value = detect_pdf(img_list, count)
+
+            return detect_value(pos, ID, value, Type, save_path, filename)
 
 
-if __name__=='__main__':
-    # dev channel 1.30
-    uvicorn.run(app='app:app',host='0.0.0.0',port=8005,reload=True)
-
+if __name__ == '__main__':
+    uvicorn.run(app='app:app', host='0.0.0.0', port=8005, reload=True)
