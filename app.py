84ea355c1d32fd315e206dae050467e0986b6857
xuecheng990531 <leexuecheng2017@outlook.com>
2023-02-02 21:17:26 +0800

    上个版本会导致识别图片格式的文件错误

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ async def ocr(
     
 
 if __name__ == '__main__':
-    uvicorn.run(app='app:app', host='127.0.0.1', port=8005, reload=True)
+    uvicorn.run(app='app:app', host='0.0.0.0', port=8005, reload=True)
