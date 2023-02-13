fb7743f35973413398d5a5f373d201aa109e2c4f
xuecheng990531 <leexuecheng2017@outlook.com>
2023-02-13 15:29:57 +0800

    update ID7

--- receipts_modules/id7.py
+++ receipts_modules/id7.py
@@ -624,7 +624,7 @@ def match_jine(pos, value, save_path):
             width = pos[i][1][0] - pos[i][0][0]
             for i in range(len(pos)):
                 if shr_pos[0][0]  < pos[i][0][0] < shr_pos[1][0] + width and shr_pos[3][1]+height*6 < pos[i][0][
-                    1] < shr_pos[3][1] + int(height * 10):
+                    1] < shr_pos[3][1] + int(height * 14):
                     if len(value[i]) != 0 and value[i][0].isdigit():
                         jine.append(value[i])
             for i in range(len(pos)):
@@ -635,7 +635,7 @@ def match_jine(pos, value, save_path):
                     for i in range(len(pos)):
                         if shr_pos[0][0] - width < pos[i][0][0] < shr_pos[1][0] and shr_pos[3][1] + height * 6 < \
                                 pos[i][0][
-                                    1] < shr_pos[3][1] + int(height * 10):
+                                    1] < shr_pos[3][1] + int(height * 14):
                             if len(value[i]) != 0 and value[i][0].isdigit():
                                 jine.append(value[i])
 
